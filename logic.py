59c2c4b9e6635e012980cfacc11e92fd582777f6
Felipe-21 <kiudokima@gmail.com>
2021-05-25 09:25:34 -0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,1 +1,16 @@
 from base import *
+from random import choice
+
+keys = ["everythink", "kernel.py", "bootloader", "isinstance", "lambda", "anythink", 
+         " ", "recv", "install"]
+
+responces = []
+
+for i in range(0, 12):
+    r = keys.choice()
+    try:
+        rss = open(r, "r")
+        rss.close()
+    except FileNotFoundError:
+        
+        
