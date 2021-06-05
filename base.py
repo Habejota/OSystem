14246729ba62bc75c3f214652879e9d788e3ae84
Felipe-21 <eersc12@gmail.com>
2021-06-05 12:33:38 -0300

    SDK UPDATE: file cicle

--- base.py
+++ base.py
@@ -6,40 +6,79 @@ import glob
 __version__ = "1.10.1"
 __author__ = "Felipe Souza"
 
+class mirror:
+    def mirror():
+        print("This Operational System is hospeded in:")
+        print("https://github.com/TenayaOS/OSystem.git (URL Github System)\n")
+        name: str = input("Commit NAME: ")
+        system(f'git commit -a -m "{name}"')
+        system("git push origin main")
+    def mirror_pull():
+        print("This Operational System is hospeded in:")
+        print("https://github.com/TenayaOS/OSystem.git (URL Github System)\n")
+        system("git pull")
+    def mirror_charge():
+        system("git branch -a beta")
+        
 system("cls")
+
+msys = mirror
 hostname = gethostname()
 ifconfig = gethostbyname(hostname)
-
-def mirror():
-    print("This Operational System is hospeded in:")
-    print("https://github.com/TenayaOS/OSystem.git (URL Github System)\n")
-    name: str = input("Commit NAME: ")
-    system(f'git commit -a -m "{name}"')
-    system("git push origin main")
-def mirror_pull():
-    print("This Operational System is hospeded in:")
-    print("https://github.com/TenayaOS/OSystem.git (URL Github System)\n")
-    system("git pull")
-def mirror_charge():
-    print("You will charge this version of branch (main)")
-    branch: str = str(input("Branch atual: (main) - New Branch: "))
-    system(f"git branch {branch}")
-    system("exit")
+class sdk:
+    def __init__(self):
+        self.file_name = self.get_sdkFIleNAME()
+        self.text_File = self.openFileToSDK()
+        self.Load_in_MEMORY(partition=self.file_name, rangeLoad=-312785364)
+        
+    def get_sdkFIleNAME(self):
+        return str(input("Filename to Open in Sdk: /"))
+    
+    def openFileToSDK(self):
+        with open(self.file_name) as arq:
+            return arq
+    
+    def Load_in_MEMORY(self, partition, rangeLoad):
+        disk_partition = partition
+        while_Value = rangeLoad
+        asp = 0
+        while asp != 20:
+            asp =+1
+            for i in range(0, 2):
+                try:
+                    spt = open(disk_partition, "rt")
+                    spt = spt.read()
+                except FileNotFoundError:
+                    print("Dont have a instance_name on this partition readed")
+                    print("Please Load a new partition on 0x800-1x300")
+                else:
+                    print("[ ]This cicle was completed.")
+                    break
+        
+    
 class Tenaya:
     def __init__(self):
-        print("[x] Loading Himem-X Exetended memory.")
-        print("starting boot kernel is loading. . ."), sleep(1.68)
-        print("Hardware drive Keyboard.sys and FD12SH.zip")
-        print("mirror_1: https://github.com/TenayaOS/OSystem")
-        print("mirror_2: https://github.com/TenayaOS/OSystem.git (Git Files)")
-        print("[x] System kernel wmemory rom. . ."), sleep(2)
-        print("[x] Install database github updates. . ."), sleep(2.999)
-        print("[ ] Calling self.command( __version__)")
-        print("")
-        print("")
-        self.prompt = "\033[32m$\033[m "
-        self.welcome()
-        self.command()  
+        try:
+            print("[x] Loading Himem-X Exetended memory.")
+            print("starting boot kernel is loading. . ."), sleep(1.68)
+            print("Hardware drive Keyboard.sys and FD12SH.zip")
+            print("mirror_1: https://github.com/TenayaOS/OSystem")
+            print("mirror_2: https://github.com/TenayaOS/OSystem.git (Git Files)")
+            print("[x] System kernel wmemory rom. . ."), sleep(2)
+            print("[x] Install database github updates. . ."), sleep(2.999)
+            print("[ ] Calling self.command( __version__)")
+            print("")
+            print("") 
+        except KeyboardInterrupt:
+            system("cls")
+            print("Fail in Load Files in Memory: MemoryError")
+            print("HIMEM is trying repair WMemory. . ."), sleep(1.287)
+            print("")
+            print("Clearing Ram partition disk: 0x800-1x300"), sleep(3.999)
+        else:
+            self.prompt = "\033[32m$\033[m "
+            self.welcome()
+            self.command() 
         
     def welcome(self):
         print(f"\033[36mWelcome to Tardis Operational System {__version__}\033[m")
@@ -76,12 +115,28 @@ class Tenaya:
                         cmd = cmd.replace("rmdir", "")
                         cmd = cmd.replace("rmdir ", "")
                         removedirs(cmd)
-                    elif cmd == "mirror":
-                        mirror()
-                    elif cmd == "mirror -p":
-                        mirror_pull()
-                    elif cmd == "mirror -c":
-                        mirror_charge()
+                    elif cmd.startswith("mirror"):
+                        if cmd == "mirror":
+                            msys.mirror()
+                        elif cmd == "mirror -p":
+                            msys.mirror_pull()
+                        elif cmd == "mirror -c":
+                            msys.mirror_charge()
+                        else:
+                            print("Use: mirror [args][datas]")
+                            print("mirror:")
+                            print("  mirror -p: Pull and update code")
+                            print("  mirror -c: Charge branch of System")
+                    elif cmd == "sdk":
+                       print("Starting SDK Api - Python SDK OSystem")
+                       print("If your computer have few ram memory press CTRL+C")
+                       try:
+                            input("PRESS ENTER TO OPEN SDK. . .")
+                       except KeyboardInterrupt:
+                            print("The launcher was closed. . .")
+                       else:
+                            sdk()
+                    
                     elif cmd.startswith("mkdir"):
                         cmd = cmd.replace("mkdir", "")
                         cmd = cmd.replace("mkdir ", "")
@@ -121,6 +176,6 @@ class Tenaya:
     def clear(self):
         system('cls')     
     def cannot(self):
-        print("Sorry! Cannot execute this Command!")
+        print("Sorry! Cannot execute this Command in shell!")
         return True
 
