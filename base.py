c0768ab4c5e19412d79115c1e90348b81573636f
Felipe-21 <eersc12@gmail.com>
2021-06-06 11:26:23 -0300

    Update base.py

--- base.py
+++ base.py
@@ -103,7 +103,23 @@ class Tenaya:
                     elif cmd == "clear" or cmd == "cls":
                         system("cls")
                     elif cmd == "ifconfig":
-                        print(f"IP: {ifconfig} (Hostname: {hostname})")
+                        print(f"""
+                                    Software Loopback Interface 1
+                                            Link encap: Local loopback
+                                            inet addr:{ifconfig} Mask: 255.0.0.0
+                                            MTU: 1500 Speed:1073,74 Mbps
+                                            Admin status:UP Oper status:OPERATIONAL
+                                            RX packets:0 dropped:0 errors:0 unkown:0
+                                            TX packets:0 dropped:0 errors:0 txqueuelen:0
+
+                                    Qualcomm Atheros QCA9377 Wireless Network Adapter
+                                            Link encap: IEEE 802.11 HWaddr: 5C-C9-D3-8D-23-9D
+                                            inet addr:{ifconfig} Mask: 255.255.255.0
+                                            MTU: 1500 Speed:108,30 Mbps
+                                            Admin status:UP Oper status:OPERATIONAL
+                                            RX packets:154204 dropped:0 errors:0 unkown:0
+                                            TX packets:106105 dropped:0 errors:0 txqueuelen:0
+                                    """)
                     elif cmd == "hostname":
                         print(f"Hostname: {hostname}")
                     elif cmd.startswith("python"):
@@ -166,8 +182,6 @@ class Tenaya:
                         cmd = cmd.replace("banner",  "")
                         cmd = cmd.replace("banner ", "")
                         system(fr"root\bin\banner.exe {cmd}")
-                    elif cmd == "ctris":
-                        system("root\bin\ctris.exe")
                     elif cmd.startswith("nano"):
                         cmd = cmd.replace("nano ", "")
                         cmd = cmd.replace("nano",  "")
