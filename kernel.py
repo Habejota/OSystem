8918f122f57b64a67fcc41fa6c16e1d6963f13e4
Felipe Souza Pereira Lima <kiudokima@gmail.com>
2021-06-24 14:33:56 -0300

    Arithimetic Pagination Terminal

--- kernel.py
+++ kernel.py
@@ -22,7 +22,7 @@ ifconfig = gethostbyname(hostname)
 port_session = randint(500, 65535)
 disk_partition = "0x800-1x300"
 version = "1.12.8" # OSYstem version
-SubVersionTAGS = "Mount Point Configuration" # SubVersion Tag
+SubVersionTAGS = "Arithimetic Pagination Terminal" # SubVersion Tag
 
 # Path go to user folder
 chdir("home")
