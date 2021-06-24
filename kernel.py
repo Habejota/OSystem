d134fcab2d50b470a32777dd32a8ce77b40a8dd8
Felipe Souza Pereira Lima <kiudokima@gmail.com>
2021-06-24 13:18:20 -0300

    Mirror Prompt

--- kernel.py
+++ kernel.py
@@ -22,7 +22,7 @@ ifconfig = gethostbyname(hostname)
 port_session = randint(500, 65535)
 disk_partition = "0x800-1x300"
 version = "1.12.8" # OSYstem version
-SubVersionTAGS = "Everythink" # SubVersion Tag
+SubVersionTAGS = "Mirror Prompt" # SubVersion Tag
 
 # Path go to user folder
 chdir("home")
@@ -33,5 +33,5 @@ print("===================================================") # Terminal desainer
 print("Welcome to OSystem v{} - {}".format(version, SubVersionTAGS)) # Version Informations
 print("OSystem: https://github.com/Habejota/OSystem.git") # Source code
 print("") # A broken line
-print("==================[OSystem Bash]===================") # Terminal desainer
+print("==================[OSystem Bash]===================") # Terminal desaine
 system(fr"{path}\os\bin\bash.exe")
