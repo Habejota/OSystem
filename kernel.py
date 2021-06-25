ba5dde340ec6b38d107acf35fc586a0381f88610
Felipe Souza Pereira de Lima <eersc12@gmail.com>
2021-06-25 17:36:50 -0300

    Native OSystem Kenrnel

--- kernel.py
+++ kernel.py
@@ -18,7 +18,7 @@ disk_partition = "0x800-1x300" # Disk Partition
 port_session = randint(500, 65535) # Session Port
  
 print("┌────────────────────────────────────────────────────────────────────┐")
-print("│Welcome to OSystem v1.13 - Source dists                             │")
+print("│Welcome to OSystem v1.13 - Native Osystem Kernel                    │")
 print("│OSystem: https://github.com/Habejota/OSystem.git                    │")
 print("│                                                                    │")
 print("└───────────────────────────[OSystem Bash]───────────────────────────┘")
