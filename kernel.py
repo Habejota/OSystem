eed4f9416356c8c4a600f39ea7ec339bcaf9b0b1
Felipe Souza Pereira de Lima <eersc12@gmail.com>
2021-06-26 12:48:13 -0300

    FirmwareShell

--- kernel.py
+++ kernel.py
@@ -1,6 +1,7 @@
 # Importing Kernel Python modules
 from os import system, getcwd, chdir
 from socket import gethostname, gethostbyname
+from time import sleep
 
 # Directory definitions
 chdir("os")
@@ -12,19 +13,25 @@ class Kernel:
         self.hostname = gethostname()
         self.host = gethostbyname(self.hostname)
         self.disk_partition = "0x800-1x300"
-        
-        self.PrintFirmwareSettings()
+        try:
+            self.PrintFirmwareSettings(), sleep(1.999)
+        except KeyboardInterrupt:
+            self._init__mensage()
+            system(r"{}\bin\sh.exe".format(path))
+        else:
+            self._init__mensage()  
+            system(r"{}\bin\bash.exe".format(path))
+    def _init__mensage(self):
         print("┌────────────────────────────────────────────────────────────────────┐")
-        print("│Welcome to OSystem v1.13 - Compile Source Code Firmware             │")
+        print("│Welcome to OSystem v1.13 - Firmware Shell                           │")
         print("│OSystem: https://github.com/Habejota/OSystem.git                    │")
         print("│                                                                    │")
         print("│                 GNU\Bash OSystem Free Source Code                  │")
         print("│        Source library free code Firmware Apache License 2.0        │")
         print("│            Binary Systems auto-compiled in scr/builtdin            │")
         print("└───────────────────────────[OSystem Bash]───────────────────────────┘")
-        system(r"{}\bin\bash.exe".format(path))
     def PrintFirmwareSettings(self):
         print("")
         system("echo Configurations firmware (%username%@{}) ({}:80)".format(self.hostname, self.host))
-        
+
 Kernel()
