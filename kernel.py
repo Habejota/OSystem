49e681d6db1a846f9cf404ad201d8e1d482a8747
Felipe Souza Pereira de Lima <eersc12@gmail.com>
2021-06-26 11:35:25 -0300

    Compile Source Code Firmware

--- kernel.py
+++ kernel.py
@@ -9,27 +9,22 @@ chdir("home")
 
 class Kernel:
     def __init__(self):
-        # Global values
         self.hostname = gethostname()
-        self.ifconfig = gethostbyname(self.hostname)
+        self.host = gethostbyname(self.hostname)
         self.disk_partition = "0x800-1x300"
-
-        # Nactive kernel started bash
-        self.__init_mensage()
-        self.execBash()
-    def __init_mensage(self):
-        print("")
-        print("Configurations firmware ({}:80)".format(self.ifconfig))
+        
+        self.PrintFirmwareSettings()
         print("┌────────────────────────────────────────────────────────────────────┐")
-        print("│Welcome to OSystem v1.13 - Firmware UPDATE                          │")
+        print("│Welcome to OSystem v1.13 - Compile Source Code Firmware             │")
         print("│OSystem: https://github.com/Habejota/OSystem.git                    │")
         print("│                                                                    │")
         print("│                 GNU\Bash OSystem Free Source Code                  │")
         print("│        Source library free code Firmware Apache License 2.0        │")
         print("│            Binary Systems auto-compiled in scr/builtdin            │")
         print("└───────────────────────────[OSystem Bash]───────────────────────────┘")
-    def execBash(self):
-        system(fr"{path}\bin\bash.exe")
-
-
+        system(r"{}\bin\bash.exe".format(path))
+    def PrintFirmwareSettings(self):
+        print("")
+        system("echo Configurations firmware (%username%@{}) ({}:80)".format(self.hostname, self.host))
+        
 Kernel()
