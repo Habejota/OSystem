df007e230bc0f094e96b300ffb4f5ad2c913b856
Felipe Souza Pereira de Lima <kiudokima@gmail.com>
2021-06-28 08:13:01 -0300

    Kernel Object Assertation

--- kernel.py
+++ kernel.py
@@ -13,21 +13,20 @@ class Kernel:
         self.hostname = gethostname()
         self.host = gethostbyname(self.hostname)
         self.disk_partition = "0x800-1x300"
-        self.version = "Command Objectives"
 
         self.FetchUpdates()
         self.PrintFirmwareSettings()
         self._init__mensage()  
         system(r"{}\bin\bash.exe".format(path))
     def _init__mensage(self):
-        print("┌────────────────────────────────────────────────────────────────────┐")
-        print("│Welcome to OSystem v1.13 - QuickBasic Update                        │")
-        print("│OSystem: https://github.com/Habejota/OSystem.git                    │")
-        print("│                                                                    │")
-        print("│                 GNU\Bash OSystem Free Source Code                  │")
-        print("│        Source library free code Firmware Apache License 2.0        │")
-        print("│            Binary Systems auto-compiled in scr/builtdin            │")
-        print("└───────────────────────────[OSystem Bash]───────────────────────────┘")
+        print("┌────────────────────────────────────────────────────────────────────┐"), sleep(0.111)
+        print("│Welcome to OSystem v1.13 - Kernel Object Assertation                │"), sleep(0.111)
+        print("│OSystem: https://github.com/Habejota/OSystem.git                    │"), sleep(0.111)
+        print("│                                                                    │"), sleep(0.111)
+        print("│                 GNU\Bash OSystem Free Source Code                  │"), sleep(0.111)
+        print("│        Source library free code Firmware Apache License 2.0        │"), sleep(0.111)
+        print("│            Binary Systems auto-compiled in scr/builtdin            │"), sleep(0.111)
+        print("└───────────────────────────[OSystem Bash]───────────────────────────┘"), sleep(0.111)
     def PrintFirmwareSettings(self):
         print("")
         system("echo Configurations firmware (%username%@{}) ({}:80)".format(self.hostname, self.host))
