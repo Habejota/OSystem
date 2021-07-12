138f2d3c987358a24b8e5bca8de802a70047dd93
Felipe Souza Pereira de Lima <eersc12@gmail.com>
2021-07-12 09:03:17 -0300

    Terminal Update: Update the distine

--- kernel.py
+++ kernel.py
@@ -19,9 +19,9 @@ class Kernel:
     def changelog(self):
         print(f"Welcome to \033[32mOSystem {self.version}\033[m (GNU\Linux - {self.subversion})")
         print("")
-        print(" * Documentation:  \033[4mhttps://github.com/Habejota/OSystem/blob/main/docs.txt\033[m")
-        print(" * Support:        (\033[4mmailto:kiudokima@gmail.com\033[m)")
-        print(" * Management:     \033[4mhttps://github.com/Habejota/OSystem\033[m")
+        print(" * Documentation:  \033[4mhttps://github.com/Habejota/OSystem/blob/main/docs.txt\033[0m")
+        print(" * Support:        \033[4mmailto:kiudokima@gmail.com\033[0m")
+        print(" * Management:     \033[4mhttps://github.com/Habejota/OSystem\033[0m")
         print("")
         print("  Ethernet WebSocket Setting:")
         print("   IPv6 address for eth4:  2001:0:2877:7aa:14d1:e090:7cff:c309")
@@ -31,7 +31,6 @@ class Kernel:
         print("Disks Firmware Devices Status on:")
         print(f"     PS/1:  \033[33mWindows NT\033[m")
         print(f"     PS/2:  \033[32mOSystem {self.version}\033[m")
-        print(f"     PS/3:  \033[36mOSystem Resources\033[m")
-        print("\033[39m=======================================\033[38m[\033[34mOSystem Bash\033[m\033[39m]=================================\033[m")
+        print("\033[36m=======================================\033[38m[\033[34mOSystem Bash\033[m\033[36m]=================================\033[m")
     
 Kernel()
